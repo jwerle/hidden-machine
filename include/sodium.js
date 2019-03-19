dbfc97ea3a3b63f05ffc013d96554cafe0f2467a
Joseph Werle <joseph.werle@gmail.com>
2019-03-19 19:22:23 -0400

    fix(include/,lib/codegen.js): Include 'hidden-machine/include' path

--- include/sodium.js
+++ include/sodium.js
@@ -1,14 +1,8 @@
 const path = require('path')
 
-const paths = [
-  path.resolve(
-    path.relative('.',
-      path.join(
-        path.dirname(require.resolve('sodium-native')),
-        path.join('libsodium', 'src', 'libsodium', 'include')
-      ))),
-
-  path.resolve(__dirname),
-]
-
-module.exports = paths.join(' ')
+module.exports = path.resolve(
+  path.relative('.',
+    path.join(
+      path.dirname(require.resolve('sodium-native')),
+      path.join('libsodium', 'src', 'libsodium', 'include')
+    )))

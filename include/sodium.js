eee66a12e9f40c19bbc7fe9f33547b18297c18c7
Joseph Werle <joseph.werle@gmail.com>
2019-03-19 19:05:11 -0400

    fix(.): Use stub 'sodium/version.h'

--- include/sodium.js
+++ include/sodium.js
@@ -1,6 +1,14 @@
-module.exports = path.resolve(
-  path.relative('.',
-    path.join(
-      path.dirname(require.resolve('sodium-native')),
-      path.join('libsodium', 'src', 'libsodium', 'include')
-    )))
+const path = require('path')
+
+const paths = [
+  path.resolve(
+    path.relative('.',
+      path.join(
+        path.dirname(require.resolve('sodium-native')),
+        path.join('libsodium', 'src', 'libsodium', 'include')
+      ))),
+
+  path.resolve(__dirname),
+]
+
+module.exports = paths.join(' ')
--- postinstall.js
+++ postinstall.js
@@ -1,14 +1,30 @@
 const { spawn } = require('child_process')
 const path = require('path')
+
 const configure = require.resolve('sodium-native/configure')
 const cwd = process.cwd()
+
+let version = null
+
+try {
+  version = require.resolve('./include/sodium/version.h')
+} catch (err) { }
+
+if (null === version) {
+  try {
+    version = require.resolve('sodium-native/libsodium/src/libsodium/include/sodium/version.h')
+  } catch (err) { }
+}
 
 const opts = {
   cwd: path.dirname(require.resolve('sodium-native')),
   stdio: 'inherit'
 }
-spawn(configure, opts, (err) => {
-  if (err) {
-    throw err
-  }
-})
+
+if (null === version) {
+  spawn(configure, opts, (err) => {
+    if (err) {
+      throw err
+    }
+  })
+}
